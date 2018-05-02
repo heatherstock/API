261bb669793ba4c3159937bb793c57ecaf302bba
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-05-02 18:59:00 +0100

    :wrench: Connect to the database

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(bodyParser.json())
 
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(dbConfig.url)
+.then (() => {
+  console.log("Successfully connected to the database");
+}).catch(err => {
+  console.log('Database connection unsuccessful');
+  process.exit();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
