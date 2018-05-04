b41fa8fd773d359cf99ff67ca3c9331902b6ddd0
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-05-04 10:11:45 +0100

    :construction: Define controllers

---
+++ app/controllers/user.controller.js
@@ -0,0 +1,21 @@
+const User = require('../models/user.model.js');
+
+exports.create = (req, res) => {
+
+};
+
+exports.findAll = (req, res) => {
+
+};
+
+exports.findOne = (req, res) => {
+
+};
+
+exports.update = (req, res) => {
+
+};
+
+exports.delete = (req, res) => {
+
+};
