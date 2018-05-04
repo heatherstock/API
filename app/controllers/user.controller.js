ba8da885734cd24ca64ace200754c9dafdd1ea29
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-05-04 10:42:37 +0100

    :sparkles: Define controller

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -2,20 +2,100 @@ const User = require('../models/user.model.js');
 
 exports.create = (req, res) => {
 
+  const user = new User({
+    forename: req.body.forename,
+    surname: req.body.surname,
+    email: req.body.email
+  });
+
+  user.save()
+  .then(data => {
+    res.send(data);
+  }).catch(err => {
+    res.status(500).send({
+      message: err.message || "An error occured while creating the user"
+    });
+  });
 };
 
 exports.findAll = (req, res) => {
 
+  User.find()
+  .then(users => {
+    res.send(users);
+  }).catch(err => {
+    res.status(500).send({
+      message: err.message || "An error occured while retrieving the user"
+    });
+  });
 };
 
 exports.findOne = (req, res) => {
 
+  User.findById(req.params.userId)
+  .then(user => {
+    if(!user) {
+      return res.status(404).send({
+        message: "User with id " + req.params.userId + " not found"
+      });
+    }
+    res.send(user);
+  }).catch(err => {
+    if(err.kind === 'ObjectId') {
+      return res.status(404).send({
+        message: "User with id " + req.params.userId + " not found"
+      });
+    }
+    return res.status(500).send({
+      message: "An error occured while retrieving user with id " + req.params.userId
+    });
+  });
 };
 
 exports.update = (req, res) => {
 
+  User.findByIdAndUpdate(req.params.userId, {
+    forename: req.body.forename,
+    surname: req.body.surname,
+    email: req.body.email
+  }, {new: true})
+  .then(user => {
+    if(!user) {
+      return res.status(404).send({
+        message: "User with id " + req.params.userId + " not found"
+      });
+    }
+    res.send(user);
+  }).catch(err => {
+    if(err.kind === 'ObjectId') {
+      return res.status(404).send({
+        message: "User with id " + req.params.userId + " not found"
+      });
+    }
+    return res.status(500).send({
+      message: "An error occured while updating user with id " + req.params.userId
+    });
+  });
 };
 
 exports.delete = (req, res) => {
 
+  User.findByIdAndRemove(req.params.noteId)
+  .then(user => {
+    if(!user) {
+      return res.status(404).send({
+        message: "User with id " + req.params.userId + " not found"
+      });
+    }
+    res.send({message: "User deleted"});
+  }).catch(err => {
+    if(err.kind === 'ObjectId') {
+      return res.status(404).send({
+        message: "User with id " + req.params.userId + " not found"
+      });
+    }
+    return res.status(500).send({
+      message: "An error occured while deleting user with id " + req.params.userId
+    });
+  });
 };
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,6 +1,6 @@
 module.exports = (app) => {
 
-  // const users = require('../controllers/user.controller.js'); reminder
+  const users = require('../controllers/user.controller.js'); 
 
   app.post('/users', users.create);
 
