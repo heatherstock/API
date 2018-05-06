e564bae22b15cf37bb70d9414c01a38d0ae0ff22
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-05-06 13:56:21 +0100

    :sparkles::white_check_mark: Render list of users details to list.ejs

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -22,8 +22,9 @@ exports.findAll = (req, res) => {
 
   User.find()
   .then(users => {
-    // res.send(users);
-    res.render('list')
+    res.render('list', { 
+      users: users
+    });
   }).catch(err => {
     res.status(500).send({
       message: err.message || "An error occured while retrieving the user"
