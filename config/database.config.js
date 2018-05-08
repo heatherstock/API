d0721e999ec08687774daaa3091bfcbe3de2ba0a
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-05-08 15:59:43 +0100

    :wrench: update mlab db info

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   url: 'mongodb://localhost:27017/WebDevAPI',
-  MONGOLAB_URI: 'mongodb://web-dev-api:web-dev-api@ds117730.mlab.com:17730/web-dev-api'
+  MONGOLAB_URI: 'mongodb://webdevapi:webdevapi@ds117730.mlab.com:17730/webdevapi'
 }
