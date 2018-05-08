dbe8a8d5f40737d1e397c14b3d7cdf5330a6204d
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-05-08 21:12:54 +0100

    :wrench: Uninstall and reinstall mongodb. Change MONGOLAB_URI

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   url: 'mongodb://localhost:27017/WebDevAPI',
-  MONGOLAB_URI: 'mongodb://webdevapi:webdevapi@ds117730.mlab.com:17730/webdevapi'
+  MONGOLAB_URI: 'mongodb://heroku_d9hjh1qk:heroku_d9hjh1qk@ds117730.mlab.com:17730/heroku_d9hjh1qk'
 }
